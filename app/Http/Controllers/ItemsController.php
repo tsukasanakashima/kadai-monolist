2e45d295d88367475c0ca46ffb39087232b9f7f0
tsukasa.nakashima <tsukasa.nakashima@rakuten.com>
2018-06-18 06:05:45 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -1,4 +1,8 @@
 <?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
 use \App\Item;
 
   class ItemsController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,5 @@ Route::group(['middleware' => ['auth']], function () {
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
     Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
     Route::resource('users', 'UsersController', ['only' => ['show']]);
+    Route::resource('items', 'UsersController', ['only' => ['show']]);
 });
